d1d19b7ceec4ea10cd1dc5dbc61bf58e767ddc4f
Michael Michel <atemhacking@gmail.com>
2020-07-09 21:38:31 +0700

    Update arch-chroot-proceed.sh

--- arch-chroot-proceed.sh
+++ arch-chroot-proceed.sh
@@ -15,15 +15,28 @@ michael ALL=(ALL:ALL) ALL
 michael ALL=(ALL:ALL) NOPASSWD: ALL
 EOT
 
+cat <<EOT > /etc/mkinitcpio.conf
+FILES=()
+HOOKS=(base udev autodetect modconf block filesystems keyboard keymap resume fsck)
+EOT
+
 cat <<EOT > /etc/default/grub
-GRUB_DEFAULT=0
 GRUB_TIMEOUT=5
 GRUB_DISTRIBUTOR="Arch"
 GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet splash root=/dev/nvme0n1p3 resume=/dev/nvme0n1p2"
 GRUB_CMDLINE_LINUX=""
+GRUB_PRELOAD_MODULES="part_gpt part_msdos"
+GRUB_TIMEOUT_STYLE=menu
+GRUB_TERMINAL_INPUT=console
+GRUB_GFXMODE=auto
+GRUB_GFXPAYLOAD_LINUX=keep
+GRUB_DISABLE_LINUX_UUID=true
+GRUB_DISABLE_RECOVERY=true
 EOT
-
 
 exec echo "LANG=en_US.UTF-8" > /etc/locale.conf
 exec ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 exec hwclock --systz --localtime
+exec mkinitcpio -P
+exec grub-install
+exec grub-mkconfig -o /boot/grub/grub.cfg
